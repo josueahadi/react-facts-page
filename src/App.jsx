4f7b57bf551bc3ec5f8b9b7caf4b7437e2649f41
josueahadi <ahadihjosue@gmail.com>
2024-07-18 16:00:28 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Main from "./components/MainContent";
+
+export default function App() {
+  return (
+    <div className="w-[550px]">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
