941f3cc56d62ed0084007dac2544684cbda84663
josueahadi <ahadihjosue@gmail.com>
2024-07-18 17:41:51 +0200

    feat: main content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 
 export default function Main() {
-  return <h1>Main component</h1>;
+  return (
+    <div className="bg-[#282D35] text-[#FFFFFF] px-5 pt-14 pb-28 relative overflow-hidden">
+      <h1 className="font-bold text-4xl">Fun Facts about React</h1>
+      <ul className="text-base font-normal list-disc list-inside pt-10 px-6 space-y-3">
+        <li className="list-item-marker">Was first released in 2013</li>
+        <li className="list-item-marker">
+          Was originally created by Jordan Walke
+        </li>
+        <li className="list-item-marker">Has well over 100K stars on GitHub</li>
+        <li className="list-item-marker">Is maintained by Meta</li>
+        <li className="list-item-marker">
+          Powers thousands of enterprise apps, including mobile apps
+        </li>
+      </ul>
+      <img
+        src="./src/assets/react-icon-big.png"
+        alt=""
+        className="absolute top-16 -right-1/4 w-64"
+      />
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between bg-[#21222A] px-5 py-6">
+    <div className="flex items-center justify-between bg-[#21222A] px-5 py-8">
       <div className="flex items-center gap-1">
         <img
           className="w-7"
@@ -10,7 +10,7 @@ export default function Navbar() {
           alt="logo"
         />
         <h3 className="font-bold text-center text-[#61DAFB] text-xl">
-          React Facts
+          ReactFacts
         </h3>
       </div>
       <h4 className="text-[#DEEBF8] text-base font-semibold">
