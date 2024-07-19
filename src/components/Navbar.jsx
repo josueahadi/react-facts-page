f1de6f3163e2f30b813da8a2fa8e4603575d839e
josueahadi <ahadihjosue@gmail.com>
2024-07-19 13:09:07 +0200

    source

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,11 +4,7 @@ export default function Navbar() {
   return (
     <div className="flex items-center justify-between bg-[#21222A] px-5 py-8">
       <div className="flex items-center gap-1">
-        <img
-          className="w-7"
-          src="./src/assets/react-icon-small.png"
-          alt="logo"
-        />
+        <img className="w-7" src="src/assets/react-icon-small.png" alt="logo" />
         <h3 className="font-bold text-center text-[#61DAFB] text-xl">
           ReactFacts
         </h3>
