d9c7a9986c5977b4f89fdad8c70149a927cb1eed
josueahadi <h.ahadi1@alustudent.com>
2024-11-01 12:51:42 +0200

    feat: implement dark mode toggle in App component and pass props to Navbar and Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import {useState} from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
